190c3375b555eb78a95cd49de746cce1f6fe0761
dnjsgml511 <dnjsgml511@naver.com>
2022-02-11 22:48:30 +0900

    테스트 코드

---
+++ Blog/src/test/java/com/portfolio/blog/board/Writing.java
@@ -0,0 +1,23 @@
+package com.portfolio.blog.board;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Writing {
+
+	@Before
+	public void before() throws Exception{
+	}
+	
+	@After
+	public void after() throws Exception{
+	}
+	
+	
+	@Test
+	public void writing() {
+		
+	}
+	
+}
