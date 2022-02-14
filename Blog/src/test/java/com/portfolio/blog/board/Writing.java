9833b14969c3845f1c83fff8f15deab1de946b79
dnjsgml511 <dnjsgml511@naver.com>
2022-02-14 23:12:18 +0900

    테스트 코드 추가

--- Blog/src/test/java/com/portfolio/blog/board/Writing.java
+++ Blog/src/test/java/com/portfolio/blog/board/Writing.java
@@ -1,23 +1,62 @@
 package com.portfolio.blog.board;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import com.portfolio.blog.board.controller.BoardController;
+import com.portfolio.blog.util.DateSetting;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 public class Writing {
 
-	@Before
-	public void before() throws Exception{
-	}
+	@Autowired
+    MockMvc mockMvc;
 	
-	@After
-	public void after() throws Exception{
+	@Autowired
+	BoardController boardController;
+	
+	@Before
+	public void before() throws Exception {
 	}
+
+	@After
+	public void after() throws Exception {
+	}
+
 	
 	
 	@Test
-	public void writing() {
+	public void boardInsertController() throws Exception {
 		
+		DateSetting ds = new DateSetting();
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("title", "title");
+		params.add("content", "content");
+		params.add("writer", "writer");
+		
+		mockMvc.perform(post("/board/insert")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+				.params(params))
+				.andDo(print())
+		 		.andExpect(status().isOk());
 	}
-	
+
 }
