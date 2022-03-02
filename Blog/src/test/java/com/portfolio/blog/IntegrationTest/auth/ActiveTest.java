b3df6d349893f98ec23adf84975cf9ba311207df
dnjsgml511 <dnjsgml511@naver.com>
2022-03-02 17:12:51 +0900

    import 수정

--- Blog/src/test/java/com/portfolio/blog/IntegrationTest/auth/ActiveTest.java
+++ Blog/src/test/java/com/portfolio/blog/IntegrationTest/auth/ActiveTest.java
@@ -21,7 +21,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.MultiValueMap;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.portfolio.blog.config.security.JwtTokenUtil;
 import com.portfolio.blog.config.security.Role;
