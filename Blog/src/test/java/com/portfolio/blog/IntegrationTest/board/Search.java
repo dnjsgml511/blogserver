0f1889efcf6c6f3fb38279bf97ccfd50cc6f1fce
dnjsgml511 <dnjsgml511@naver.com>
2022-03-03 17:31:26 +0900

    옮기는 작업중

--- Blog/src/test/java/com/portfolio/blog/IntegrationTest/board/Search.java
+++ Blog/src/test/java/com/portfolio/blog/IntegrationTest/board/Search.java
@@ -1,152 +1,231 @@
 package com.portfolio.blog.IntegrationTest.board;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
+import javax.transaction.Transactional;
+
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
-import com.portfolio.blog.config.security.JwtTokenUtil;
-import com.portfolio.blog.config.security.Role;
-import com.portfolio.blog.data.controller.BoardController;
-import com.portfolio.blog.data.entitiy.BoardEntity;
 import com.portfolio.blog.data.entitiy.UserEntity;
-import com.portfolio.blog.data.repository.BoardRepository;
 import com.portfolio.blog.data.repository.UserRepository;
+import com.portfolio.blog.util.BeforeAllSetting;
 import com.portfolio.blog.util.ControllerMockPerform;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
-public class Search extends ControllerMockPerform{
+@Transactional
+class Search extends ControllerMockPerform{
 
-	@Autowired
-    MockMvc mockMvc;
+	@Autowired MockMvc mockMvc;
+	@Autowired BeforeAllSetting beforeAllSetting;
+	@Autowired UserRepository userRepository;
 	
-	@Autowired
-	BoardController boardController;
-	
-	@Autowired
-	BoardRepository boardRepository;
-	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	JwtTokenUtil jwtTokenUtil;
-	String token;
-	UserEntity user;
-	List<UserEntity> userlist;
-	
-	@Before
+	@BeforeAll
 	public void before() throws Exception {
-		
-		HashMap<String, Object> claims = new HashMap<String, Object>();
-		claims.put("role", Role.ROLE_ADMIN);
-		token = jwtTokenUtil.generateToken("ADMIN", claims);
-		
-		userlist = new ArrayList<UserEntity>();
-		user = new UserEntity("admin", "관리자", "1234", Role.ROLE_ADMIN);
-		userlist.add(user);
-		user = new UserEntity("user", "사용자", "1234");
-		userlist.add(user);
-		user = new UserEntity("user2", "두번째", "1234");
-		userlist.add(user);
-		user = new UserEntity("asdf", "사용자2", "1234");
-		userlist.add(user);
-		userRepository.saveAll(userlist);
-		
-		List<BoardEntity> list = new ArrayList<BoardEntity>();
-		for(int i = 0; i < 5; ++i) {
-			BoardEntity set = new BoardEntity("top title" + i, "top content" + i, "user");
-			set.setTop(1);
-			list.add(set);
+		userRepository.saveAll(beforeAllSetting.userSetting());
+		List<UserEntity> test = userRepository.findAll();
+		for (UserEntity data : test) {
+			System.out.println(data);
 		}
-		
-		for(int i = 0; i < 5; ++i) {
-			BoardEntity set = new BoardEntity("title" + i, "content" + i, "user");
-			list.add(set);
-		}
-		for(int i = 0; i < 5; ++i) {
-			BoardEntity set = new BoardEntity("title" + i, "content" + i, "user");
-			set.setHide(1);
-			list.add(set);
-		}
-		
-		for(int i = 0; i < 5; ++i) {
-			BoardEntity set = new BoardEntity("title" + i, "content" + i, "user");
-			list.add(set);
-		}
-		for(int i = 0; i < 5; ++i) {
-			BoardEntity set = new BoardEntity("nexttitle" + i, "content" + i, "user");
-			list.add(set);
-		}
-		boardRepository.saveAll(list);
 	}
 
-	@After
+	@AfterAll
 	public void after() throws Exception {
-		boardRepository.deleteAll();
 	}
 	
-	@Test
-	public void boardNoPageSearchController() throws Exception {
+	@Nested
+	@DisplayName("검색 테스트")
+	public class SearchTest {
 		
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("size", "15");
+		@Test
+		@DisplayName("관리자 기본검색")
+		void defaultAdminSearch() throws Exception {
+			List<UserEntity> test = userRepository.findAll();
+			for (UserEntity data : test) {
+				System.out.println(data);
+			}
+		}
 		
-		getMockMVC("/board/search/user", params, status().isOk(), token);
+		@Test
+		@DisplayName("유저 기본검색")
+		void defaultUserSearch() throws Exception {
+			System.out.println("test2");
+		}
 	}
 	
-	@Test
-	public void boardNoSizeSearchController() throws Exception {
-		
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("page", "0");
-		
-		getMockMVC("/board/search/user", params, status().isOk(), token);
-	}
 	
-	@Test
-	public void boardNoSearchController() throws Exception {
-		
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("page", "0");
-		params.add("size", "15");
-		
-		getMockMVC("/board/search/user", params, status().isOk(), token);
-	}
 	
-	@Test
-	public void boardSearchController() throws Exception {
-		
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("page", "0");
-		params.add("size", "15");
-		params.add("search", "next");
-		
-		getMockMVC("/board/search/user", params, status().isOk(), token);
-	}
 	
-	@Test
-	public void boardTopSearchController() throws Exception {
-		
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("size", "3");
-		
-		getMockMVC("/board/topsearch/user", params, status().isOk(), token);
-		
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Test
+//	@DisplayName("검색x, 사용자변경")
+//	public void noSearchChangeUserController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		
+//		getMockMVC("/board/search/user", params, status().isOk(), ADMIN_TOKEN);
+//	}
+//	
+//	@Test
+//	@DisplayName("관리자x, 검색x")
+//	public void noAdminNoSearchChangeUserController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/user", params, status().isOk(), USER_TOKEN);
+//	}
+//	
+//	@Test
+//	@DisplayName("검색x, 사용자 미변경")
+//	public void noSearchNotChangeUserController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), ADMIN_TOKEN);
+//	}
+//	
+//	@Test
+//	@DisplayName("관리자x, 검색x")
+//	public void noAdminNoSearchNotChangeUserController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), USER_TOKEN);
+//	}
+//	
+//	@Test
+//	@DisplayName("검색x, 사용자 미변경, 페이지 없음")
+//	public void noSearchNotChangeUserNoPageController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), ADMIN_TOKEN);
+//	}
+//	
+//	@Test
+//	@DisplayName("검색x, 사용자 미변경, 페이지크기 없음")
+//	public void noSearchNotChangeUserNoSizeController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), ADMIN_TOKEN);
+//	}
+//	
+//	
+//	@Test
+//	@DisplayName("검색x, 사용자 미변경, 페이지크기 없음, 사이즈 없음")
+//	public void noSearchNotChangeUserNoPageNoSizeController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), ADMIN_TOKEN);
+//	}
+	
+	
+	
+	
+	
+//	
+//	
+//	
+//	
+//	
+//	@Test
+//	public void boardNoPageSearchController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search", params, status().isOk(), token);
+//	}
+//	
+//	@Test
+//	public void boardNoSizeSearchController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		
+//		getMockMVC("/board/search", params, status().isOk(), token);
+//	}
+//	
+//	@Test
+//	public void boardNoSearchAllDataController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/admin", params, status().isOk(), token);
+//	}
+//	
+//	@Test
+//	public void boardNoSearchController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		
+//		getMockMVC("/board/search/user", params, status().isOk(), token);
+//	}
+//	
+//	@Test
+//	public void boardSearchController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("page", "0");
+//		params.add("size", "15");
+//		params.add("search", "next");
+//		
+//		getMockMVC("/board/search/user", params, status().isOk(), token);
+//	}
+//	
+//	@Test
+//	public void boardTopSearchController() throws Exception {
+//		
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//		params.add("size", "3");
+//		
+//		getMockMVC("/board/topsearch", params, status().isOk(), token);
+//		
+//	}
 
 }
